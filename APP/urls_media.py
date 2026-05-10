b5bd0921db8cd854aa51d4b7d5a0e5e6e8ce960c
Juan Pablo Molina <jpm.juanmolina@gmail.com>
2026-05-09 22:13:41 -0400

    added url media entry to manual upload to s3

--- APP/urls_media.py
+++ APP/urls_media.py
@@ -4,4 +4,5 @@ from . import views_media
 urlpatterns = [
     path('compressed/', views_media.compression_callback, name='compression_callback'),
     path('upload/', views_media.create_multimedia, name='create_multimedia'),
+    path('get-upload-url/', views_media.get_upload_url, name='get_upload_url'),
 ]
